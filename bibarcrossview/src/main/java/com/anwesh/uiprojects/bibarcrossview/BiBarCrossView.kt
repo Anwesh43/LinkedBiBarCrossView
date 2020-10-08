435b33ed70c91ddf09e7471295e3715d8f6e9c35
Anwesh43 <anweshthecool0@gmail.com>
2020-10-09 03:14:50 +0530

    adding BiBarCrossView class

--- bibarcrossview/src/main/java/com/anwesh/uiprojects/bibarcrossview/BiBarCrossView.kt
+++ bibarcrossview/src/main/java/com/anwesh/uiprojects/bibarcrossview/BiBarCrossView.kt
@@ -61,3 +61,19 @@ fun Canvas.drawBBCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiBarCross(scale, w, h, paint)
 }
+
+class BiBarCrossView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
